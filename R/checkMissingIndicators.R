9bf297492283508135e912ddcdf05a77c980a012
aagret <aagret@petray.ch>
2018-06-01 15:12:31 +0200

    check database for missing datas and try to update

---
+++ R/checkMissingIndicators.R
@@ -0,0 +1,16 @@
+
+
+checkMissingIndicators <- (db= database) {
+    
+        
+    m_db <- db[, lapply(.SD, function(x) all(is.na(x))), by= Ticker]
+    m_db <- melt(m_db, id=1)
+    m_db <- m_db[value == TRUE]
+    m_db <- split(m_db$Ticker, f= m_db$variable, drop = TRUE)
+    
+    dts <- unique(db$Date)
+    
+    res <- lapply(m_db, function(x) getBdhData(unlist(x), names(x), dts))
+    
+}
+
