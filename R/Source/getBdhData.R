d15f9cea6605991c9e46d2b1a0fb3d0368dab31a
aagret <aagret@petray.ch>
2018-06-01 15:07:29 +0200

    modif to adapt to data publication frequency and reduce feed consumption

--- R/Source/getBdhData.R
+++ R/Source/getBdhData.R
@@ -2,40 +2,76 @@
 # get blooberg historical datas
 
 getBdhData <- function(tic= missingTic, flds= fields, dts= weekDts)
-    
-    if (length(tic) > 0 ) {
-        
-        con <- blpConnect()
-        
-        opt = c("periodicityAdjustment"="CALENDAR",
-                "periodicitySelection"="WEEKLY",
-                "nonTradingDayFillMethod"="PREVIOUS_VALUE")
 
-#       opt = c("periodicitySelection"="WEEKLY")
+    if (length(tic) > 0 ) {
+
+        flds <- indicatorFields[indicatorFields$Field %in% flds,]
+        flds <- split(flds$Field, f= factor(flds$Period))
+
+        con <- blpConnect()
+
+        # get daily datas   
+        optBase = c("periodicityAdjustment"="CALENDAR",
+                    "nonTradingDayFillOption"="ACTIVE_DAYS_ONLY",
+                    "nonTradingDayFillMethod"="PREVIOUS_VALUE")
         
-        db <- bdh(tic, 
-                  flds, 
-                  start.date= dts[1],
-                  end.date= last(dts),
-                  include.non.trading.days = TRUE,
-                  options= opt,
-                  verbose= TRUE)
+        
+        if (length(as.character(flds$D)) > 0) {
+            
+            opt <- c(optBase, "periodicitySelection"="WEEKLY")
+            
+            db_d <- bdh(tic, 
+                        as.character(flds$D), 
+                        start.date= dts[1],
+                        end.date= last(dts),
+                        include.non.trading.days = TRUE,
+                        options= opt,
+                        verbose= TRUE)
+            
+            # set to data.frame
+            if (is.data.frame(db_d)) db_d <- cbind(.id=tic, db_d) else db_d <- ldply(db_d, data.frame)
+            
+            blpMax <<- blpMax - length(which(!is.na(db_d[-1])))
+        }
+            
+        # get periodicity datas
+        tic <- cbind(tic, 
+                     substring(securities[Ticker %in% tic, Primary_Periodicity], 1, 1))
+        
+        tic <- split(tic[,1], f= tic[,2], drop= TRUE)
+        
+        opt <- c(optBase, "periodicitySelection"="QUARTERLY")
         
         
+        # get periodicity datas
+        if (length(as.character(flds$F)) > 0) {
+            
+            db_f <- lapply(tic, function(x) bdh(x, 
+                                                as.character(flds$F), 
+                                                start.date= dts[1],
+                                                end.date= last(dts),
+                                                include.non.trading.days = TRUE,
+                                                options= opt,
+                                                verbose= TRUE))
+            
+            db_f <- ldply(db_f, function(x) ldply(x, data.frame))
+            
+            
+            blpMax <<- blpMax - length(which(!is.na(db_f[-1])))
+            
+        }
         
         blpDisconnect(con)
         
-        # set to data.frame
-        if (length(tic) > 1) db <- ldply(db, data.frame) else db <- cbind(tic, db)
+        if (exists("db_d")) db <- db_d else db <- NULL
+        if (exists("db_f")) db <- merge(db, db_f, by= c(".id", "date"), all=TRUE)
+        
+        db$X..i.. <- NULL
         
         colnames(db)[1:2] <- c("Ticker", "Date")
         
-        # add calculated fields
-        if(length(db$T12M_NET_CAPITAL_STOCK) > 0) db <- calculatedFields(db)
-        
         setDT(db, key= c("Ticker", "Date"))
         
-        db <- db[, lapply(.SD, function(x) na.locf(x, na.rm= FALSE)), by= Ticker]
         
     }
 
