173706d1cbfe915dec6c5fb2584ccf5fbeb0e79f
aagret <aagret@petray.ch>
2018-06-01 15:05:50 +0200

    function to summarize database

---
+++ R/Source/getSummary.R
@@ -0,0 +1,18 @@
+
+#### Statistiques de la Database ####
+
+getSummary <- function(db= database) {
+    
+    stats <- matrix(, nrow = 7, ncol = ncol(db))
+    stats <- rbind(Min.=     sapply(db, min,            na.rm=TRUE),
+                   '1st Qu.'=sapply(db, quantile, 0.25, na.rm=TRUE),
+                   Median=   sapply(db, median,         na.rm=TRUE),
+                   Mean=     sapply(db, mean,           na.rm=TRUE),
+                   '3rd Qu.'=sapply(db, quantile, 0.75, na.rm=TRUE),
+                   MAx.=     sapply(db, max,            na.rm=TRUE),
+                   'NA'=     sapply(db, function(x) sum(is.na(x))))
+    
+    round(stats, 2)
+   
+
+}
