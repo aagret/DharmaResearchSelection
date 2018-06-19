cb082e32de9cdd5201bb65896eca12ddec1ba55e
aagret <aagret@petray.ch>
2018-06-19 08:23:16 +0200

    TODO not operationnal

--- R/checkMissingIndicators.R
+++ R/checkMissingIndicators.R
@@ -2,8 +2,7 @@
 
 checkMissingIndicators <- (db= database) {
     
-        
-    m_db <- db[, lapply(.SD, function(x) all(is.na(x))), by= Ticker]
+    m_db <- db[, lapply(.SD, function(x) all(is.na(x))), by= .(Ticker, Date)]
     m_db <- melt(m_db, id=1)
     m_db <- m_db[value == TRUE]
     m_db <- split(m_db$Ticker, f= m_db$variable, drop = TRUE)
@@ -11,6 +10,6 @@ checkMissingIndicators <- (db= database) {
     dts <- unique(db$Date)
     
     res <- lapply(m_db, function(x) getBdhData(unlist(x), names(x), dts))
-    
+
 }
 
